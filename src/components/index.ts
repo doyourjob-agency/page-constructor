efa63b9bc8ab3435516fa1cd68a09cfd6a5da134
0viliniw0 <bklaj98@gmail.com>
2025-11-20 19:12:30 +0400

    feat: add banner-minify block and blockUnicorn props

--- src/components/index.ts
+++ src/components/index.ts
@@ -48,6 +48,7 @@ export {default as ReportSection} from './ReportSection/ReportSection';
 export {default as ReportItem} from './ReportItem/ReportItem';
 export {default as Odometer} from './Odometer/Odometer';
 export {default as BackgroundEffect} from './BackgroundEffect/BackgroundEffect';
+export {default as BackgroundUnicorn} from './BackgroundUnicorn/BackgroundUnicorn';
 
 export type {RouterLinkProps} from './RouterLink/RouterLink';
 export type {ImageBaseProps} from './ImageBase/ImageBase';
