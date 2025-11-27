c32c3d8d8e297b895bffd609447f3aefccee7544
0viliniw0 <bklaj98@gmail.com>
2025-11-27 14:58:54 +0400

    chore: bump v5.31.142-dev.6

--- src/blocks/Benefits/schema.ts
+++ src/blocks/Benefits/schema.ts
@@ -57,7 +57,6 @@ export const BenefitsBlock = {
             },
             theme: ThemeProps,
             accentColor: {type: 'string'},
-            time: {type: 'number'},
         },
     },
 };
