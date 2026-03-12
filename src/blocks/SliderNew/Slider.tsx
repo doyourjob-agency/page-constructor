f0add13458609d2d033486ff5ac1071ebacec7ca
g4ps <magnificent.eugene@gmail.com>
2026-03-12 13:13:10 +0000

    feat: make slider have height on ssr

--- src/blocks/SliderNew/Slider.tsx
+++ src/blocks/SliderNew/Slider.tsx
@@ -1,6 +1,12 @@
 import React, {ComponentType, PropsWithChildren, useEffect, useState} from 'react';
 
+import {block} from '../../utils';
+
 import type {SliderNewProps} from './SliderClient';
+
+import './Slider.scss';
+
+const b = block('slider-new-block');
 
 export const SliderNewBlock = (props: PropsWithChildren<SliderNewProps>) => {
     const [SliderClient, setSliderClient] = useState<ComponentType<SliderNewProps> | null>(null);
@@ -11,7 +17,7 @@ export const SliderNewBlock = (props: PropsWithChildren<SliderNewProps>) => {
         });
     }, []);
 
-    if (!SliderClient) return null;
+    if (!SliderClient) return <div className={b('server-side-placeholder')}>{props.children}</div>;
 
     return <SliderClient {...props} />;
 };
