6f7dae42142147ce197b53737b9eda114d31487d
0viliniw0 <0viliniw0@users.noreply.github.com>
2026-04-02 09:54:46 +0000

    deploy: ccce7457b3fcf2ece407cbd817dfa2673678768b

---
+++ main/assets/ReactPlayer.stories-C20nyfjM.js
@@ -0,0 +1,5 @@
+import{R as e}from"./iframe-DBz9FxxA.js";import{R as s}from"./ReactPlayer-ztIiRcEN.js";import"./preload-helper-D6kgxu3v.js";import"./debounce-CZCzfD7y.js";import"./isSymbol-BOoKSYuc.js";import"./cjs-Cz8BQk8L.js";import"./index-CD3JR4Ag.js";import"./addComponentKeysets-B7lkJpNs.js";import"./common-CgL19bot.js";import"./Icon-BaW8Dr_8.js";import"./useMount-BZCMgbLQ.js";import"./useAnalytics-DxpSjvP8.js";import"./PlayFill-DD5IHH9w.js";const m={default:{content:{src:"https://doyourjob-agency.github.io/page-constructor/main/story-assets/video_8-12_white.mp4",loop:!0,customBarControlsClassName:"",showPreview:!0}}},_={component:s,title:"Components/Pics, video, DataLens/ReactPlayer"},c=i=>e.createElement("div",{style:{maxWidth:"500px"}},e.createElement(s,{...i})),t=c.bind({});t.args=m.default.content;var o,r,a;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`args => <div style={{
+  maxWidth: '500px'
+}}>
+        <ReactPlayer {...args} />
+    </div>`,...(a=(r=t.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const D=["Default"];export{t as Default,D as __namedExportsOrder,_ as default};
---
+++ main/assets/useMount-BZCMgbLQ.js
@@ -0,0 +1,1 @@
+import{r as o}from"./iframe-DBz9FxxA.js";function e(t){o.useEffect(t,[])}export{e as u};
