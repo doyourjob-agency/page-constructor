13345e023875f135b5cccd0be0a4cdaefd1aea9b
0viliniw0 <0viliniw0@users.noreply.github.com>
2026-06-08 10:23:18 +0000

    deploy: e41263b34b8bd5a7647321d1b76d34db9fe93a13

--- main/assets/ConstructorRow-8efCHaSt.js
+++
@@ -1,1 +0,0 @@
-import{b as r,R as t}from"./iframe-BPLGfZvu.js";import{R as s}from"./Row-BRsPjbeG.js";import{C as e}from"./Col-bjMvaJ7q.js";const a=r("constructor-row"),c=({children:o})=>o?t.createElement(s,{className:a()},t.createElement(e,null,o)):null;c.__docgenInfo={description:"",methods:[],displayName:"ConstructorRow"};export{c as C};
---
+++ main/assets/Title-BwMnVASS.js
@@ -0,0 +1,18 @@
+import{j as e,M as o}from"./iframe-D0yTPwpH.js";import{useMDXComponents as n}from"./index-CpPHOyxZ.js";import{S as s}from"./StoryTemplate-43fGPAdv.js";import{T as l}from"./Title.stories-BlTYpwhE.js";import"./preload-helper-D6kgxu3v.js";import"./utils-B07n3FGj.js";import"./index-CgbWzPKm.js";import"./keys-DolPcJKY.js";import"./_baseKeys-DxnTVP1v.js";import"./url-BoOgGw1q.js";import"./index-DBmYSTEC.js";import"./cjs-Cz8BQk8L.js";import"./Title-CAtniB9D.js";import"./YFMWrapper-BYp--q9V.js";import"./HTML-Dx3Ky5ra.js";import"./blocks-DWtPoJIW.js";import"./toString-B5CuaVYL.js";import"./isSymbol-Dz6CxyvU.js";import"./ToggleArrow-CPB6TO5g.js";import"./svg-CQ-OHbUx.js";import"./Icon-D3-5kvwR.js";import"./url-CdJE1tRu.js";import"./Link-CjQEkLka.js";import"./BackLink-lFcMuQv1.js";import"./useAnalytics-3WZpjiv1.js";import"./Button-ffdfd3AE.js";import"./common-BNr4zm1e.js";import"./EventBroker-Bx85hY8r.js";import"./FileLink-DFlOGJic.js";import"./Label-D4leL1Cb.js";import"./Xmark-XK9_4GPR.js";import"./Col-B0zJUd6t.js";function r(i){const t={code:"code",em:"em",h2:"h2",li:"li",p:"p",strong:"strong",ul:"ul",...n(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(o,{of:l}),`
+`,e.jsxs(s,{children:[e.jsx(t.h2,{id:"parameters",children:"Parameters"}),e.jsx(t.p,{children:e.jsx(t.code,{children:"subtitleBold?: boolean"})}),e.jsx(t.p,{children:e.jsx(t.code,{children:"subtitle?: text"})}),e.jsx(t.p,{children:e.jsx(t.code,{children:"title:"})}),e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[`
+`,e.jsxs(t.p,{children:[e.jsx(t.code,{children:"text: string"})," - Title text"]}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsxs(t.p,{children:[e.jsx(t.code,{children:"textSize?: 's' | 'm' | 'l' | 'xl'"})," — Title font size"]}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(t.p,{children:e.jsx(t.code,{children:"centered?: boolean"})}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsxs(t.p,{children:[e.jsx(t.code,{children:"url?: string"})," — URL for a redirect on clicking the title, an arrow is automatically added at the end."]}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsxs(t.p,{children:[e.jsx(t.code,{children:"resetMargin?: boolean"})," - default ",e.jsx(t.code,{children:"true"}),". Without this property ",e.jsx(t.code,{children:"margin-top"})," will be proportional to ",e.jsx(t.code,{children:"textSize"})," (see section ",e.jsx(t.em,{children:"Margins"})," below)"]}),`
+`]}),`
+`]}),e.jsxs(t.p,{children:[e.jsx(t.code,{children:"description: string"})," - text (with YFM support)"]}),e.jsx(t.p,{children:e.jsx(t.strong,{children:"Margins for title without reset:"})}),e.jsx(t.p,{children:e.jsx(t.code,{children:"textSize s - top: m"})}),e.jsx(t.p,{children:e.jsx(t.code,{children:"textSize m - top: l"})}),e.jsx(t.p,{children:e.jsx(t.code,{children:"textSize l - top: xl"})}),e.jsx(t.p,{children:e.jsx(t.code,{children:"textSize xl - top: xl"})})]})]})}function q(i={}){const{wrapper:t}={...n(),...i.components};return t?e.jsx(t,{...i,children:e.jsx(r,{...i})}):r(i)}export{q as default};
